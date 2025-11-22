bb0c5cdc90502e0245c620fc207e602ca949c546
Chris Wright <chrisw3010@outlook.com>
2025-11-22 11:47:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const port = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from Azure App Service!\n');
+  res.end('Hello from Azure App Service! CI/CD working!\n');
 }).listen(port, () => console.log(`Server running on port ${port}`));
